aa6525afa2c658555bd154c1b9d8cc421778c22f
vmiinv <a.kadirov.17.06@gmail.com>
2021-11-12 00:21:14 +0500

    filter, security

--- src/main/java/com/example/zuulserv/ZuulServApplication.java
+++ src/main/java/com/example/zuulserv/ZuulServApplication.java
@@ -1,9 +1,11 @@
 package com.example.zuulserv;
 
+import com.example.zuulserv.filters.BaseFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +16,8 @@ public class ZuulServApplication {
         SpringApplication.run(ZuulServApplication.class, args);
     }
 
+    @Bean
+    public BaseFilter baseFilter() {
+        return new BaseFilter();
+    }
 }
