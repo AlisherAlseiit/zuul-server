d937970713bc773afdff8b1ec8d7ea361b837aed
L-Law-Liet <a.kadirov.17.06@gmail.com>
2021-11-30 01:01:37 +0500

    integration with frontend

--- src/main/java/com/example/zuulserv/security/SecurityTokenConfig.java
+++ src/main/java/com/example/zuulserv/security/SecurityTokenConfig.java
@@ -2,7 +2,7 @@ package com.example.zuulserv.security;
 
 import javax.servlet.http.HttpServletResponse;
 
-import com.example.zuulserv.filters.JwtTokenAuthenticationFilter;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -11,9 +11,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity 	// Enable security config. This annotation denotes config for spring security.
 public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String[] AUTH_WHITELIST = {
+			// -- Swagger UI v2
+			"/v2/api-docs",
+			"/swagger-resources",
+			"/swagger-resources/**",
+			"/configuration/ui",
+			"/configuration/security",
+			"/swagger-ui.html",
+			"/webjars/**",
+			// -- Swagger UI v3 (OpenAPI)
+			"/v3/api-docs/**",
+			"/swagger-ui/**"
+	};
 
 	@Autowired
 	private JwtConfig jwtConfig;
@@ -22,17 +39,39 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
     	   http
 		.csrf().disable()
 				   .authorizeRequests()
-				   .antMatchers("/auth/**").permitAll()
-				   .antMatchers("/api/ratings/**").hasAuthority("company")
+				   .antMatchers(AUTH_WHITELIST).permitAll()
+				   .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+				   .antMatchers("/ratings/**").permitAll()
 				   .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
 		    // handle an authorized attempts
-		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-		   .and().addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class);;
+		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+		http.cors();
 	}
 
 	@Bean
 	public JwtConfig jwtConfig() {
 		return new JwtConfig();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(ImmutableList.of("*"));
+		configuration.setAllowedMethods(ImmutableList.of("HEAD",
+				"GET", "POST", "PUT", "DELETE", "PATCH"));
+		// setAllowCredentials(true) is important, otherwise:
+		// The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+//        configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(ImmutableList.of(
+				"Authorization",
+				"Cache-Control",
+				"Content-Type"
+		));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
